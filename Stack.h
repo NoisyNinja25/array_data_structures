b8e48356525b602a43a93c0356de3bc0b34cc2fa
CarsonP25 <thebubbag@gmail.com>
2025-09-20 21:35:20 -0400

    comments for stack

--- Stack.h
+++ Stack.h
@@ -19,18 +19,41 @@ class Stack : public Array<T> {
         ~Stack();
 
 
+        /**
+         * push an element onto the stack
+         * 
+         * @param[in] el: the element to be pushed
+        */
         void push(T el);
 
 
+        /**
+         * remove and return the top element from the stack
+        */
         T pop();
 
         
+        /**
+         * test if the stack is empty
+         * 
+         * @retval true if stack's array size is zero
+         * @retval false otherwise
+        */
         bool is_empty(); 
 
-        
+        /**
+         * remove all elements from the stack
+         * resizes the allocation to zero
+        */
         void clear();
 
 
+        /**
+         * assignment operator
+         * 
+         * @param[in] rhs: the stack on the right hand side 
+         *                 of the equation
+        */
         const Stack & operator = (const Stack & rhs);
 
     private:
